9227f8f5c0efce2375625884297b86753ce19e79
italocw <italobezerra.cw@gmail.com>
2024-04-08 10:16:00 -0300

    App bar now uses app localizations text to get the app title

--- lib/features/authentication/presentation/components/templates/auth_template.dart
+++ lib/features/authentication/presentation/components/templates/auth_template.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:home_activity_suggestions/core/components/organisms/custom_app_bar.dart';
+import 'package:home_activity_suggestions/core/providers.dart';
 import 'package:home_activity_suggestions/features/authentication/presentation/components/organisms/auth_organism.dart';
 
-class AuthTemplate extends StatelessWidget {
+class AuthTemplate extends ConsumerWidget {
   const AuthTemplate({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+  Widget build(BuildContext context,  WidgetRef ref) {
+    final appTitle = ref.read(appLocalizationsProvider).app_title;
+    return  Scaffold(
       appBar: CustomAppBar(
-        titleText: 'Sugestões de atividades caseiras',
+        titleText:appTitle
       ),
-      body:AuthOrganism(),
+      body:const AuthOrganism(),
     );
   }
 }
--- lib/features/suggestions/presentation/components/templates/categories_template.dart
+++ lib/features/suggestions/presentation/components/templates/categories_template.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:home_activity_suggestions/core/providers.dart';
 import 'package:home_activity_suggestions/features/suggestions/presentation/components/organisms/categories_list.dart';
 
+import '../../../../../core/components/organisms/custom_app_bar.dart';
 import '../../../data/suggestion_categories.dart';
 
 class CategoriesTemplate extends ConsumerWidget {
@@ -10,7 +12,10 @@ class CategoriesTemplate extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final categories = ref.read(categoriesProvider);
-    return Scaffold(
+    final appTitle = ref.read(appLocalizationsProvider).app_title;
+    return  Scaffold(
+      appBar: CustomAppBar(
+          titleText:appTitle),
       body: CategoriesList(categories: categories),
     );
   }
